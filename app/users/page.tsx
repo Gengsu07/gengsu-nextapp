2326e502a93cac381ad6597038f56b6a9e0e4ee2
Sugeng Wahyudi <sugengdatascience@gmail.com>
2023-09-20 06:27:40 +0700

    d

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,33 +1,45 @@
-import React from "react";
+import Link from "next/link";
+import { sort } from "fast-sort";
 
 interface User {
   id: number;
   name: string;
   email: string;
 }
+interface Props {
+  searchParams: { sortOrder: string };
+}
 
-const UsersPage = async () => {
+const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
     next: { revalidate: 60 },
   });
   const users: User[] = await res.json();
+  const sortedUsers = sort(users).asc(
+    sortOrder === "email" ? (u) => u.email : (u) => u.name
+  );
 
   return (
     <>
       <h1>UsersPage</h1>
+      <p> sorted by: {sortOrder}</p>
       <table className="table">
         <thead>
           <tr>
             <th>
-              <h3 className="font-bold text-black">Nama</h3>
+              <Link href={"/users?sortOrder=name"}>
+                <h3 className="font-bold text-black">Nama</h3>
+              </Link>
             </th>
             <th>
-              <h3 className="font-bold text-black">Email</h3>
+              <Link href={"/users?sortOrder=email"}>
+                <h3 className="font-bold text-black">Email</h3>
+              </Link>
             </th>
           </tr>
         </thead>
         <tbody>
-          {users.map((user) => (
+          {sortedUsers.map((user) => (
             <tr key={user.id} className="hover">
               <td>{user.name}</td>
               <td>{user.email}</td>
